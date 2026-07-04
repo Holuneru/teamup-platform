88a7433f5743ad698148d0a99c3085ee2d72b74d
Ivan <ivanmalikov2302@gmail.com>
2026-07-04 12:16:33 +0300

    INIT

---
+++ src/main/java/com/example/teamupplatform/TeamupPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamupplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamupPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamupPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/teamupplatform/TeamupPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamupplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamupPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
